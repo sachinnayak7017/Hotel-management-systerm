4bc149a161b5f7a2d11fe1b8a516532ec1c5db2e
sachinnayak7017 <sachinnayak7017@gmail.com>
2024-03-02 09:21:07 +0530

    hotel management system

---
+++ src/hotel/management/system/Conn.java
@@ -0,0 +1,23 @@
+package hotel.management.system;
+
+
+
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/HotelManagement","root",""); 
+
+            s =c.createStatement();  
+
+
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
